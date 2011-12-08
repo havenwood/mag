0262a6998c3216089723ce6694583c9709a1dc40
Shannon <shannonskipper@gmail.com>
2011-12-07 16:05:29 -0800

    added aliases for common commands, in the Ruby tradition

--- lib/mag.rb
+++ lib/mag.rb
@@ -5,17 +5,25 @@ class Mag
     def commit
       Maglev.commit_transaction
     end
+    
+    alias push commit
   
     def abort
       Maglev.abort_transaction
     end
+    
+    alias pull abort
   
     def box
       Maglev::PERSISTENT_ROOT
     end
     
+    alias store box
+    
     def box= this
       Maglev::PERSISTENT_ROOT.replace this
     end
+    
+    alias store= box=
   end
 end
